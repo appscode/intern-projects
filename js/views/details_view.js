a5dd8e0627e688102cb2cc276a4da0dcf6c497cc
Emruz-Hossain <hossainemruz@gmail.com>
2016-11-14 18:37:05 +0600

    details vew2

--- js/views/details_view.js
+++ js/views/details_view.js
@@ -4,8 +4,32 @@ Backbone.$ = $;
 const ENTER_KEY = 13;
 const ESC_KEY = 27;
 
-module.exports = Backbone.View.extend({
-	
-	tagName: 'div',
-	template: 'details.html'
+var BookModel = Backbone.Model.extend({
+	defaults: {
+		image: '../Book_Images/l1.jpg'
+		title:'Linux programming by example',
+		author: 'Arnold Robbins',
+		edition: '1st',
+		category: 'Computer Science',
+		price: '$300',
+		overview: 'Linux Programming By Example introduces programmers with some background in C but no knowledge of the specifics of Linux programming to the fundamentals of Linux system programming and application development. Topics covered include using GNU development tools, system programming, file handling in Linux, interprocess communication, network programming, application programming interfaces, debugging and memory management, and version control and software distribution.'
+	}
 });
+
+var BookView = Backbone.View.extend({
+
+	tagName: 'div',
+	template: _.template($('#details_template').html()),
+
+	initialize: function(){
+		this.render();
+	}, 
+
+	render: function(){
+		this.el.html(this.template(this.model.toJSON()));
+	}
+
+});
+
+var book = new BookModel;
+var bookView = new BookView({model: book});
